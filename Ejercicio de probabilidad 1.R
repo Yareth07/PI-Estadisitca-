28c735c3973b6c590a67b7da041f248a01fae3d6
Yareth07 <100616875+Yareth07@users.noreply.github.com>
2022-05-27 15:14:34 -0500

    Ejercicios de probabilidad
    Script para calcular la probabilidad

---
+++ Ejercicio de probabilidad 1.R
@@ -0,0 +1,12 @@
+# Ejercicio de probabilidad, calcular la probabilidad de X sea menor o igual a 48
+pnorm(48, mean= 50, sd= sqrt(25))
+
+# Ejercicio de probabilidad, calcular la probabilidad de X sea menor o igual a 50
+pnorm(50, mean= 52, sd= sqrt(25))
+
+# Ejercicio de probabilidad, calcular la probabilidad de X sea menor o igual a 50
+pnorm(38, mean= 42, sd= sqrt(19))
+
+
+# Busqueda de ejercicios, se adjunta el link de la plataforma 
+# https://rpubs.com/CarlosLopez123/631357 
